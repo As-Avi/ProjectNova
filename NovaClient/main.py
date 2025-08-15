d60c1ff5da84d00b178cfac394f13e715546c493
marco <marco.altomare@silca.it>
2025-08-15 15:39:49 +0200

    Tolto parametro langiage

--- NovaClient/main.py
+++ NovaClient/main.py
@@ -3,6 +3,7 @@ from io import StringIO
 import os
 from dotenv import load_dotenv
 import argparse
+import locale
 
 from novaClient import App
 
@@ -10,17 +11,35 @@ from novaClient import App
 if __name__ == "__main__":
     print(sys.argv)
 
-    #leggo i parametri dal file .env
+    # leggo i parametri dal file .env
     load_dotenv()
-    
+
     parser = argparse.ArgumentParser(description="Parametri da inserire.")
-   
-    parser.add_argument("-m", "--modulo", type=str, required=True, help="Modulo 0 = VIEW | 1 = FILTEREDVIEW [obbligatorio]")
-    parser.add_argument("-u", "--url", type=str, required=True, help="URL del server [obbligatorio]")
-    parser.add_argument("-f", "--file", type=str, required=True, help="File JSON di configurazione [obbligatorio]")
-    parser.add_argument("-l", "--language", type=str, required=False, help="Lingua del progetto [opzionale, default: 'it']", default="it")
+
+    parser.add_argument(
+        "-m",
+        "--modulo",
+        type=str,
+        required=True,
+        help="Modulo 0 = VIEW | 1 = FILTEREDVIEW [obbligatorio]",
+    )
+    parser.add_argument(
+        "-u", "--url", type=str, required=True, help="URL del server [obbligatorio]"
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        required=True,
+        help="File JSON di configurazione [obbligatorio]",
+    )
 
     args = parser.parse_args()
 
+    # Default settings based on the user's environment.
+    locale.setlocale(locale.LC_ALL, "")
+    language, _ = locale.getlocale()
+    user_lang = language.split("_")[0]
+
     # creo la form
-    App(args.modulo, args.url, args.file,  args.language)
+    App(args.modulo, args.url, args.file, user_lang)
